0b702aadd74a48af57f95fd4745efb4938b12728
Clifford Grimmell and Gloria Friesen <clifford@>
2017-03-14 09:05:21 -0700

    add CD class and CD ArrayList

---
+++ CD.java
@@ -0,0 +1,13 @@
+class CD {
+  public String mAlbum;
+  public String mArtist;
+  public int mYear;
+  public Integer mPrice;
+}
+
+public CD(String album, String artist, int year, Integer price){
+  mAlbum = album;
+  mArtist = artist;
+  mYear = year;
+  mPrice = price;
+}
